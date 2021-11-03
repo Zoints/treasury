bbb8b8bf41820cf8b80af78097132e636c5c1511
WhoSoup <who.soup@gmail.com>
2021-11-03 14:01:21 +0100

    launch script

---
+++ test/scripts/launch.ts
@@ -0,0 +1,30 @@
+import {
+    Cluster,
+    clusterApiUrl,
+    Connection,
+    Keypair,
+    PublicKey,
+    sendAndConfirmTransaction,
+    Transaction
+} from '@solana/web3.js';
+import { TreasuryInstruction } from '@zoints/treasury';
+
+(async () => {
+    let url = process.env.URL || 'http://localhost:8899';
+    try {
+        url = clusterApiUrl(process.env.URL as Cluster);
+    } catch (e) {}
+
+    const programId = new PublicKey(process.env.PROGRAM_ID || 'error');
+    const mint = new PublicKey(process.env.MINT || 'error');
+    const funder = Keypair.fromSecretKey(
+        Buffer.from(process.env.FUNDER || 'error', 'hex')
+    );
+    const connection = new Connection(url, 'confirmed');
+
+    const tx = new Transaction().add(
+        await TreasuryInstruction.Initialize(programId, funder.publicKey, mint)
+    );
+    console.log('Sending transaction...');
+    console.log(await sendAndConfirmTransaction(connection, tx, [funder]));
+})().then(() => process.exit(0));
